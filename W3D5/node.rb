3d8a4886c338ef136e2db98da953895e055bfc11
Christian <christianday954@ymail.com>
2023-02-17 11:32:31 -0800

    Add nodes and tree visualization

--- W3D5/node.rb
+++ W3D5/node.rb
@@ -1,10 +1,23 @@
 class Node
+    
     attr_reader :value, :children
-
-    #node has info on itself and its children (will be emtpy by default)
     def initialize(value, children = [])
+        #node has info on itself and its children (will be emtpy by default)
         @value = value
         @children = children
     end
 end
 
+d = Node.new("d")
+e = Node.new("e")
+f = Node.new("f")
+g = Node.new("g")
+b = Node.new("b", [d, e]) #the node instances will be children
+c = Node.new("c", [f, g])
+a = Node.new("a", [b, c])
+
+#     a
+#    /  \
+#   b    c
+# /  \  /  \
+# d   e f   g 
