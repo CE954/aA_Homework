c4b3e177c829fd690304e36eb2f98e285023ddec
Christian <christianday954@ymail.com>
2023-03-01 22:34:18 -0800

    Fix syntax error

--- W5D4/intro_project/app/models/person.rb
+++ W5D4/intro_project/app/models/person.rb
@@ -11,7 +11,7 @@
 class Person < ApplicationRecord
     validates :name, presence: true 
 
-    belongs_to :house
+    belongs_to :house,
     primary_key: :id, 
     foreign_key: :house_id, 
     class_name: :House
